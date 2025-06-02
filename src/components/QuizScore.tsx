2092f74c3426b1b7a1744be4b954d5b12758db57
gayeongX0916 <gayeongx0916@gmail.com>
2025-06-02 13:52:21 +0900

    [feature] 컴포넌트 분리화

---
+++ src/components/QuizScore.tsx
@@ -0,0 +1,5 @@
+const QuizScore = () => {
+  return <div className="quiz-score">점수 : </div>;
+};
+
+export default QuizScore;
---
+++ src/components/QuizSection.tsx
@@ -0,0 +1,17 @@
+const QuizSection = () => {
+  return (
+    <>
+      <h4>문제 : 1/10</h4>
+      <h4>주제 : 영화</h4>
+      <div className="quiz-question">문제 내용</div>
+      <div className="quiz-button-container">
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>4</button>
+      </div>
+    </>
+  );
+};
+
+export default QuizSection;
